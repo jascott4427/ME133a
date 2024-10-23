3f3b2f2ded0e1c66dc7e26d112ef80a64b009054
James A Scott III <111306950+jascott4427@users.noreply.github.com>
2024-10-23 02:39:43 -0700

    Create hw3p6.py

---
+++ src/hw3code/hw3code/hw3p6.py
@@ -0,0 +1,128 @@
+import rclpy
+import numpy as np
+
+from math               import pi, sin, cos, acos, atan2, sqrt, fmod
+
+from rclpy.node         import Node
+from sensor_msgs.msg    import JointState
+
+
+#
+#   Trajectory Class
+#
+class Trajectory():
+    # Initialization.
+    def __init__(self):
+        pass
+
+    # Declare the joint names.
+    def jointnames(self):
+        # Return a list of joint names MATCHING THE JOINT NAMES IN THE URDF!
+        return ['theta1', 'theta2', 'theta3', 'theta4']
+
+    # Evaluate at the given time and time step since last call.
+    def evaluate(self, t, dt):
+        # Compute the joint values.  We could build up numpy arrays as
+        # position and velocity vectors, but just compute the values:
+        theta_1  = (pi / 3) * sin(2* t)
+        omega_1  = ((2*pi)/3)*cos(2*t)
+
+        theta_2 = (pi/3)*sin(t)-(pi/9)*cos(6*t)
+        omega_2 = (pi/3)*cos(t)+(6*pi/9)*sin(6*t)
+
+        theta_3  = (pi / 6) * sin(2* t)
+        omega_3  = ((2*pi)/6)*cos(2*t)
+
+        theta_4 = (pi/6)*sin(t)-(pi/9)*cos(6*t)
+        omega_4 = (pi/6)*cos(t)+(6*pi/9)*sin(6*t)
+
+        # Return the position and velocity as python lists.
+        return ([theta_1, theta_2, theta_3, theta_4], [omega_1, omega_2, omega_3, omega_4])
+
+
+#
+#   Generator Node Class
+#
+#   This inherits all the standard ROS node stuff, but adds an
+#   update() method to be called regularly by an internal timer and a
+#   shutdown method to stop the timer.
+#
+#   Take the node name and the update frequency as arguments.
+#
+class Generator(Node):
+    # Initialization.
+    def __init__(self, name, rate):
+        # Initialize the node, naming it 'generator'
+        super().__init__(name)
+
+        # Set up the trajectory.
+        self.trajectory = Trajectory()
+        self.jointnames = self.trajectory.jointnames()
+
+        # Add a publisher to send the joint commands.
+        self.pub = self.create_publisher(JointState, '/joint_states', 10)
+
+        # Wait for a connection to happen.  This isn't necessary, but
+        # means we don't start until the rest of the system is ready.
+        self.get_logger().info("Waiting for a /joint_states subscriber...")
+        while(not self.count_subscribers('/joint_states')):
+            pass
+
+        # Set up the timing so (t=0) will occur in the first update
+        # cycle (dt) from now.
+        self.dt    = 1.0 / float(rate)
+        self.t     = -self.dt
+        self.start = self.get_clock().now()+rclpy.time.Duration(seconds=self.dt)
+
+        # Create a timer to trigger calculating/sending commands.
+        self.timer = self.create_timer(self.dt, self.update)
+        self.get_logger().info("Running with dt of %f seconds (%fHz)" %
+                               (self.dt, rate))
+
+    # Shutdown
+    def shutdown(self):
+        # Destroy the timer, then shut down the node.
+        self.timer.destroy()
+        self.destroy_node()
+
+    # Update - send a new joint command every time step.
+    def update(self):
+        # To avoid any time jitter enforce a constant time step and
+        # integrate to get the current time.
+        self.t += self.dt
+
+        # Compute the desired joint positions and velocities for this time.
+        (q, qdot) = self.trajectory.evaluate(self.t, self.dt)
+
+        # Determine the corresponding ROS time (seconds since 1970).
+        now = self.start + rclpy.time.Duration(seconds=self.t)
+
+        # Build up a command message and publish.
+        cmdmsg = JointState()
+        cmdmsg.header.stamp = now.to_msg()      # Current time for ROS
+        cmdmsg.name         = self.jointnames   # List of joint names
+        cmdmsg.position     = q                 # List of joint positions
+        cmdmsg.velocity     = qdot              # List of joint velocities
+        self.pub.publish(cmdmsg)
+
+
+#
+#  Main Code
+#
+def main(args=None):
+    # Initialize ROS.
+    rclpy.init(args=args)
+
+    # Initialize the generator node for 100Hz udpates.
+    generator = Generator('generator', 100)
+
+    # Spin, meaning keep running (taking care of the timer callbacks
+    # and message passing), until interrupted.
+    rclpy.spin(generator)
+
+    # Shutdown the node and ROS.
+    generator.shutdown()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
