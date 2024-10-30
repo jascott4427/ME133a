220a39b37cc6939c1ba6ee5b2ca0df69bad9ea73
firehawk4427 <firehawk4427@gmail.com>
2024-10-29 22:22:24 -0700

    update

--- src/hw4code/hw4code/hw4p1.py
+++ src/hw4code/hw4code/hw4p1.py
@@ -10,33 +10,35 @@
 import numpy as np
 
 
-#
+#   
 #  Forward Kinematics
 #
 def fkin(q):
-    # EDIT THIS CODE TO DO SOMETHING USEFUL!
-    print("Put the forward kinematics here")
-    x = np.array([1,
-                  2,
-                  3])
-
-    # Return the tip position as a NumPy 3-element vector.
-    return x
+    thetap, theta1, theta2 = q
+    x = -np.sin(thetap)*(np.cos(theta1)+np.cos(theta1+theta2))
+    y = np.cos(thetap)*(np.cos(theta1)+np.cos(theta1+theta2))
+    z = np.sin(theta1)+np.sin(theta1+theta2)
+    return np.array([x, y, z])
 
 
 #
 #  Jacobian
 #
 def Jac(q):
-    # EDIT THIS CODE TO DO SOMETHING USEFUL!
-    print("Put the Jacobian here")
-    J = np.eye(3)
-    J = np.random.rand(3,3)
-    J = np.array([[1, 2, 3],
-                  [4, 5, 6],
-                  [7, 8, 9]])
+    thetap, theta1, theta2 = q
+    j00 = -np.cos(thetap)*(np.cos(theta1)+np.cos(theta1+theta2))
+    j01 = np.sin(thetap)*(np.sin(theta1)+np.sin(theta1+theta2))
+    j02 = np.sin(thetap)*np.sin(theta1+theta2)
+    j10 = -np.sin(thetap)*(np.cos(theta1)+np.cos(theta1+theta2))
+    j11 = -np.cos(thetap)*(np.sin(theta1)+np.sin(theta1+theta2))
+    j12 = -np.cos(thetap)*np.sin(theta1+theta2)
+    j20 = 0
+    j21 = np.cos(theta1)+np.cos(theta1+theta2)
+    j22 = np.cos(theta1+theta2)
 
-    # Return the Jacobian as a NumPy 3x3 matrix.
+    J = np.array([[j00, j01, j02],
+                  [j10, j11, j12],
+                  [j20, j21, j22]])
     return J
 
 
@@ -51,17 +53,17 @@ def main():
 
     # First (given) test case with following joint coordinates.
     print("TEST CASE #1:")
-    q = np.radians(np.array([20, 40, -30]))
-    print('q:\n',       q)
-    print('fkin(q):\n', fkin(q))
-    print('Jac(q):\n',  Jac(q))
+    q1 = np.radians(np.array([20, 40, -30]))
+    print('q:\n',       q1)
+    print('fkin(q):\n', fkin(q1))
+    print('Jac(q):\n',  Jac(q1))
 
     # Second test case with following joint coordinates.
     print("TEST CASE #2")
-    q = np.radians(np.array([30, 20, 50]))
-    print('q:\n',       q)
-    print('fkin(q):\n', fkin(q))
-    print('Jac(q):\n',  Jac(q))
+    q2 = np.radians(np.array([30, 20, 50]))
+    print('q:\n',       q2)
+    print('fkin(q):\n', fkin(q2))
+    print('Jac(q):\n',  Jac(q2))
 
 if __name__ == "__main__":
     main()
